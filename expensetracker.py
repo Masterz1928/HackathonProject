ff2d895ed5c74801efb240cea975905df3f80797
Harsimran <hsj030609@gmail.com>
2025-09-06 12:04:46 +0800

    Added in the receipt input option

--- expensetracker.py
+++ expensetracker.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import customtkinter as ctk
 import pytesseract
+from tkinter import filedialog
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 root = ctk.CTk()
 root.title("Expense Tracker")
@@ -9,10 +11,25 @@ root.geometry("750x550")
 label = ctk.CTkLabel(root, text="Expense Tracker", font=("Helvetica", 20))
 label.pack()
 
+
+def select_receipt():
+    filepath = filedialog.askopenfilename(
+        initialdir="/",
+        title="Select a file",
+        filetypes=(("Image files", "*.png;*.jpg;*.jpeg"), ("All files", "*.*"))
+        )
+    if filepath:
+        img = Image.open(filepath)
+        text = pytesseract.image_to_string(img)
+        Output.delete("1.0", "end")
+        Output.insert("end", text)
+    
+select_recipt_button = ctk.CTkButton(root, text="Select a recipt", font=("Helventica", 20), command=select_receipt)
+select_recipt_button.pack()
+
+Output = ctk.CTkTextbox(root, width=500, height=300)
+Output.pack()
+
+
 root.mainloop()
 
-
-img = Image.open("moni.jpg")  # put a small receipt image here
-text = pytesseract.image_to_string(img)
-print("---- OCR Output ----")
-print(text)
